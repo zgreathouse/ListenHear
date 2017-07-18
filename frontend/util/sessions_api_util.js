ccbdcdfbd783c585ee87ceb15e2262e95173d3d4
zgreathouse <greathouse.zachary@gmail.com>
2017-07-18 10:53:12 -0700

    Front end auth:user model, users controller, sessions, controller, application controller, staticpages controller, staticpages root.html.erb(view), sessions api/util, frontend entry file

---
+++ frontend/util/sessions_api_util.js
@@ -0,0 +1,27 @@
+import {
+  receiveCurrentUser,
+  receiveErrors
+} from '../actions/session_actions';
+
+export const signin = user => (
+  $.ajax({
+    method: 'POST',
+    url: '/api/session',
+    data: user
+  })
+);
+
+export const signup = user => (
+  $.ajax({
+    method: 'POST',
+    url: '/api/user',
+    data: user
+  })
+);
+
+export const signout = () => (
+  $.ajax({
+    method: 'DELETE',
+    url: '/api/session'
+  })
+);
