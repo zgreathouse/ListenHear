71bc540ad8ef443fa4b0196140887f6504dfe2f4
zgreathouse <greathouse.zachary@gmail.com>
2017-07-22 15:42:09 -0700

    refactor songs_index_item component

--- frontend/components/songs/songs_index_item.jsx
+++ frontend/components/songs/songs_index_item.jsx
@@ -7,9 +7,7 @@ class SongIndexItem extends React.Component {
 
     return (
       <li className="song-index-item">
-        <Link to={`/songs/${song.id}`}>
-          <img className="song-index-image" height="100" width="100" src={song.avatar} alt="cover-photo" />
-        </Link>
+        <img className="song-image" height="100" width="100" src={song.avatar} alt="cover-photo" />
       </li>
     );
   }
