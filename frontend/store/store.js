3938f07e9f2941ca8059afb17f69ff79c47528d9
zgreathouse <greathouse.zachary@gmail.com>
2017-07-28 14:12:51 -0700

    changed files to avoid console errors

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,15 +1,23 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import RootReducer from '../reducers/root_reducer';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const createLogger = require('redux-logger');
+  middlewares.push(createLogger());
+}
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer,
     preloadedState,
-    composeWithDevTools(applyMiddleware(thunk, logger))
+    composeWithDevTools(applyMiddleware(...middlewares))
   )
 );
 
